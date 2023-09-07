d0aae4d7ca1826eafe1a7a8191d9cebe79e7c8bf
Hamzin Daulet <qdis009@gmail.com>
2023-09-07 16:46:24 +0600

    added personValidator

--- src/main/java/kz/example/demo/dao/PersonDAO.java
+++ src/main/java/kz/example/demo/dao/PersonDAO.java
@@ -13,20 +13,34 @@ import java.util.Optional;
 public class PersonDAO {
     @Autowired
     private JdbcTemplate jdbcTemplate;
-    public List<Person> index(){
-        return jdbcTemplate.query("select *from person",new BeanPropertyRowMapper<>(Person.class));
+
+    public List<Person> index() {
+        return jdbcTemplate.query("select *from person", new BeanPropertyRowMapper<>(Person.class));
     }
-    public Optional<Person> show(int id){
-        return jdbcTemplate.query("select *from person where id=?",new Object[]{id},new BeanPropertyRowMapper<>(Person.class))
+
+    public Optional<Person> show(int id) {
+        return jdbcTemplate.query("select *from person where id=?", new Object[]{id}, new BeanPropertyRowMapper<>(Person.class))
                 .stream().findAny();
     }
-    public void save(Person person){
-        jdbcTemplate.update("insert into person(fullName,age,email,address)",new Object[]{person.getFullName(),person.getAge(),person.getEmail(),person.getAddress()});
+
+    public void save(Person person) {
+        jdbcTemplate.update("insert into person(fullName,age,email,address)", new Object[]{person.getFullName(), person.getAge(), person.getEmail(), person.getAddress()});
     }
-    public void update(int id,Person updatedperson){
-        jdbcTemplate.update("update person set fullName=?,age=?,email=?,address=? where id=?",new Object[]{updatedperson.getFullName(),updatedperson.getAge(),updatedperson.getEmail(),updatedperson.getAddress(),id});
+
+    public void update(int id, Person updatedperson) {
+        jdbcTemplate.update("update person set fullName=?,age=?,email=?,address=? where id=?", new Object[]{updatedperson.getFullName(), updatedperson.getAge(), updatedperson.getEmail(), updatedperson.getAddress(), id});
     }
-    public void delete(int id){
-        jdbcTemplate.update("delete from person where id=?",new Object[]{id});
+
+    public void delete(int id) {
+        jdbcTemplate.update("delete from person where id=?", new Object[]{id});
+    }
+
+    public Optional<Person> getPersonByFullName(String fullName) {
+        return jdbcTemplate.query("select *from person where fullName=?", new Object[]{fullName}, new BeanPropertyRowMapper<>(Person.class))
+                .stream().findAny();
+    }
+    public Optional<Person> getPersonByEmail(String email){
+        return jdbcTemplate.query("select *from person where email=",new Object[]{email},new BeanPropertyRowMapper<>(Person.class))
+                .stream().findAny();
     }
 }
---
+++ src/main/java/kz/example/demo/util/PersonValidator.java
@@ -0,0 +1,30 @@
+package kz.example.demo.util;
+
+import kz.example.demo.dao.PersonDAO;
+import kz.example.demo.models.Person;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Component;
+import org.springframework.validation.Errors;
+import org.springframework.validation.Validator;
+
+@Component
+public class PersonValidator implements Validator {
+    @Autowired
+    PersonDAO personDAO;
+
+    @Override
+    public boolean supports(Class<?> clazz) {
+        return Person.class.equals(clazz);
+    }
+
+    @Override
+    public void validate(Object target, Errors errors) {
+        Person person=(Person) target;
+        if(personDAO.getPersonByFullName(person.getFullName()).isPresent()){
+            errors.rejectValue("fullName","","Человек с таким именем уже существует");
+        }
+        if(personDAO.getPersonByEmail(person.getEmail()).isPresent()){
+            errors.rejectValue("email","","Данный email уже зарегистрирован в базе данных");
+        }
+    }
+}
