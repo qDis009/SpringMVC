96c49a9372befc9c42c7a774fe818bcecac93505
Hamzin Daulet <qdis009@gmail.com>
2023-09-08 18:41:03 +0600

    updated bookDAO

--- src/main/java/kz/example/demo/dao/BookDAO.java
+++ src/main/java/kz/example/demo/dao/BookDAO.java
@@ -1,6 +1,7 @@
 package kz.example.demo.dao;
 
 import kz.example.demo.models.Book;
+import kz.example.demo.models.Person;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.jdbc.core.BeanPropertyRowMapper;
 import org.springframework.jdbc.core.JdbcTemplate;
@@ -26,8 +27,20 @@ public class BookDAO {
     public void update(int id,Book updatedbook){
         jdbcTemplate.update("update book set title=?,author=?,year=? where id=?",new Object[]{updatedbook.getTitle(),updatedbook.getAuthor(),updatedbook.getYear(),id});
     }
-    public void delete(int id){
-        jdbcTemplate.update("delete from book where id=?",new Object[]{id});
+    public void delete(int id) {
+        jdbcTemplate.update("delete from book where id=?", new Object[]{id});
     }
-
+    //Находим владельца книги по его id
+    public Optional<Person> getBookOwner(int id){
+        return jdbcTemplate.query("select person.* from book join person on book book.person_id=person.id where book.id=?",new Object[]{id},new BeanPropertyRowMapper<>(Person.class))
+                .stream().findAny();
+    }
+    //Этот медот освождает книгу когда человек возвращает её
+    public void release(int id){
+        jdbcTemplate.update("update book set person_id=null where id=?",new Object[]{id});
+    }
+    //Назначает книгу человеку когда он берет её из библиотеки
+    public void assign(int id, Person selectedPerson){
+        jdbcTemplate.update("update book set person_id=? where id=?",new Object[]{selectedPerson.getId(),id});
+    }
 }
